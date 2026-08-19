7c235c1ac524fbcb08d162edecb87031b578162b
Artem <artwhite2012@gmail.com>
2026-08-19 15:04:13 +0300

    No-Name buffer solution

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,14 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local buffers = vim.api.nvim_list_bufs()
+    for _, bufnr in ipairs(buffers) do
+      if vim.api.nvim_buf_get_name(bufnr) == "" and vim.bo[bufnr].buftype == "" and not vim.bo[bufnr].modified then
+        vim.api.nvim_buf_delete(bufnr, { force = true })
+      end
+    end
+  end,
+})
